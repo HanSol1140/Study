4b4733595c00219c64336d4231d3c429f205fc47
송한솔 <skycriperd@gmail.com>
2023-06-12 19:03:56 +0900

    274

--- Java/코드/JavaBasic/Java05/InnerClassModifier1/src/Main.java
+++ Java/코드/JavaBasic/Java05/InnerClassModifier1/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        class InstanceInner{
+            int iv = 100;
+            //**<!-- static int cv = 100; 내부 클래스중에서 static 클래스만 static 멤버를 가질 수 있다. -->**/
+            final static int CONST = 100;
+        }
+
+        static class StaticInner{
+            int iv = 200;
+            static int cv = 200;
+        }
+        void myMethod(){
+            class LocalInner{}
+        }
     }
 }
