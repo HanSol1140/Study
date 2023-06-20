05c926da6b57c59a645ed18640b60323216215ac
SongHanSol <skycriperd@gmail.com>
2023-06-20 18:55:17 +0900

    111

--- Z-CodingTest/python/note.py
+++ Z-CodingTest/python/note.py
@@ -1,6 +1,12 @@
-import math
+import requests
 
-n = 976
-n = math.sqrt(n)
-print(n)
-print(1 if isinstance(n, int) else 2)
+# ESP32의 IP 주소
+esp32_ip = '192.168.0.9'
+
+# ESP32에 GET 요청 보내기
+response = requests.get(f'http://{esp32_ip}/control?pin=19&state=1')
+
+if response.status_code == 200:
+    print('요청이 성공적으로 전달되었습니다.')
+else:
+    print('요청 전달 실패')
