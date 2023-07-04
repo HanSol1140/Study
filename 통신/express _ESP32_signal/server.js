d72d4a7a483c0694415a88efa997be29bd7e48ec
SongHanSol <skycriperd@gmail.com>
2023-07-04 16:25:44 +0900

    11

--- 통신/express _ESP32_signal/server.js
+++ 통신/express _ESP32_signal/server.js
@@ -7,22 +7,22 @@ app.use(express.static('public'));
 app.use(express.json());
 // 8083/send로 접속하면 해당 명렁어 실행
 
+// const response = await axios.post(`http://1.212.172.134:8082/?id=${req.body.id}&pass=${req.body.pass}&callbackurl=${req.body.callbackurl}&callbackhost=${req.body.callbackhost}&callbackport=${req.body.callbackport}`);
 app.get('/send', async function (req, res) {
     try {
-        // const response = await axios.post(`http://1.212.172.134:8082/?id=${req.body.id}&pass=${req.body.pass}&callbackurl=${req.body.callbackurl}&callbackhost=${req.body.callbackhost}&callbackport=${req.body.callbackport}`);
-        const response = await axios.get(`http://1.212.172.134:8082/setringcallback.html?sender=01031277711`);
-            console.log()
+        const response = await axios.get(`http://1.212.172.134:8082/nanonix.html?sender=01031277711`);
+            console.log("전송완료");
         if (response.status === 200) {
             console.log(response.data);
         }
       } catch (error) {
         console.error('Error with API call:', error);
-        res.send("error");
+        res.send("error");  
       }
 });
 
 
 
-app.listen(8085, function () {
-  console.log('포트 8083 서버실행 완료');
+app.listen(8084, function () {
+  console.log('포트 8084 서버실행 완료');
 });
