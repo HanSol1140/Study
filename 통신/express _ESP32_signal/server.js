fc2b0b51b7ca0d83a7a138373e4b7f156a1c1839
SongHanSol <skycriperd@gmail.com>
2023-07-04 14:10:01 +0900

    11

---
+++ 통신/express _ESP32_signal/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const axios = require('axios');
+app.use(cors()); // 모든 도메인에서의 요청 허용
+app.use(express.static('public'));
+app.use(express.json());
+// 8083/send로 접속하면 해당 명렁어 실행
+
+app.get('/send', async function (req, res) {
+    try {
+        // const response = await axios.post(`http://1.212.172.134:8082/?id=${req.body.id}&pass=${req.body.pass}&callbackurl=${req.body.callbackurl}&callbackhost=${req.body.callbackhost}&callbackport=${req.body.callbackport}`);
+        const response = await axios.get(`http://1.212.172.134:8082/setringcallback.html?sender=01031277711`);
+            console.log()
+        if (response.status === 200) {
+            console.log(response.data);
+        }
+      } catch (error) {
+        console.error('Error with API call:', error);
+        res.send("error");
+      }
+});
+
+
+
+app.listen(8085, function () {
+  console.log('포트 8083 서버실행 완료');
+});
