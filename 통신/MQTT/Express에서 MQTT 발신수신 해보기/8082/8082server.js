3a2778a660b157f30ec03d3383e2af97b7e5080d
SongHanSol <skycriperd@gmail.com>
2023-07-13 17:58:59 +0900

    1

--- 통신/MQTT/Express에서 MQTT 발신수신 해보기/8082/8082server.js
+++ 통신/MQTT/Express에서 MQTT 발신수신 해보기/8082/8082server.js
@@ -29,9 +29,9 @@ client.on('reconnect', function() {
 app.get('/send', function (req, res) { // JSON 형식으로 요청
   // json 형식으로 message 생성
   var message = {
-    planName: "advertplan002, advertplan001, advertplan003, 123",
+    planName: "advertplan001",
     cMarksNames : "nnx10",
-    marqueName : "test1, test2"
+    marqueName : "test0"
   };
   client.publish('outTopic', JSON.stringify(message));
   res.send('Message sent to MQTT broker');
