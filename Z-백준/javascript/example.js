877a97329cfc29c3392794972288094d32ecb7e4
songhansol <skycriperd@gmail.com>
2023-12-11 17:42:04 +0900

    123

---
+++ Z-백준/javascript/example.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+// var input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+var input = fs.readFileSync('C:/songhansol/Study/Z-백준/javascript/input.txt').toString().trim().split("\r\n");
+console.log(input);
+
+
+
