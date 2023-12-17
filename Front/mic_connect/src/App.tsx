2f800c7988adc6cf822a40a8c811d90f93c2c00d
송한솔 <skycriperd@gmail.com>
2023-12-18 02:26:05 +0900

    mic_test

---
+++ Front/mic_connect/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AudioRecorder from './component/AudioRecorder';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <AudioRecorder></AudioRecorder>
+    </div>
+  );
+}
+
+export default App;
---
+++ Front/mic_connect/src/component/AudioRecorder.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+const AudioRecorder = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
+  const [audioUrl, setAudioUrl] = useState<string | null>(null);
+
+  const startRecording = async () => {
+    setIsRecording(true);
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const recorder = new MediaRecorder(stream);
+    setMediaRecorder(recorder);
+
+    const audioChunks: BlobPart[] = [];
+    recorder.ondataavailable = (event) => {
+      audioChunks.push(event.data);
+    };
+
+    recorder.onstop = () => {
+      setIsRecording(false);
+      const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+      const url = URL.createObjectURL(audioBlob);
+      setAudioUrl(url);
+    };
+
+    recorder.start();
+  };
+
+  const stopRecording = () => {
+    mediaRecorder?.stop();
+  };
+
+  const downloadRecording = () => {
+    if (audioUrl) {
+      const link = document.createElement('a');
+      link.href = audioUrl;
+      link.download = 'recording.wav';
+      link.click();
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={startRecording} disabled={isRecording} style={{ backgroundColor: isRecording ? 'red' : 'green' }}>
+        {isRecording ? 'Recording...' : 'Start Recording'}
+      </button>
+      <button onClick={stopRecording} disabled={!isRecording}>
+        Stop Recording
+      </button>
+      <button onClick={downloadRecording}>
+        Download Recording
+      </button>
+
+      {isRecording && <div>🎤 녹음 중...</div>}
+    </div>
+  );
+};
+
+export default AudioRecorder;
