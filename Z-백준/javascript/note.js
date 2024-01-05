4e856bd6876c4ef9a0ce797fb189426308c99e54
songhansol <skycriperd@gmail.com>
2024-01-05 18:35:14 +0900

    11

--- Z-백준/javascript/note.js
+++ Z-백준/javascript/note.js
@@ -1,49 +1,13 @@
-// 방법 1
-// var input = 75;
 
-// switch (true) {
-//     case (input >= 60 && input <= 69):
-//         var input = 123 + ((150-123)/10*(input-60));
-//         console.log(input);
-//         break;
-//     case (input >= 70 && input <= 79):
-//         var input = 150 + ((175-150)/10*(input-70));
-//         console.log(input);
-//         break;
-//     // 다른 조건들...
-//     default:
-//         console.log('Value is out of range');
-// }
+let pattern = new RegExp(/\d{4}\/\d{2}\/\d{2}/, 'g');
+let dateS = '2020/02/02'
 
-// 방법 2
-// var input = 85;
+console.log(dateS) // 2020/02/02
+console.log(pattern.test(dateS)); // true
+console.log(pattern.test(dateS)); // true
 
-// var ranges = [
-//     { min: 60, max: 69, startValue: 123, endValue: 150 },
-//     { min: 70, max: 79, startValue: 150, endValue: 175 },
-//     { min: 80, max: 89, startValue: 175, endValue: 104 },
-
-// ];
-
-// var result;
-
-// for (let range of ranges) {
-//     if (input >= range.min && input <= range.max) {
-//         result =
-//             range.startValue + ((range.endValue - range.startValue)
-//             / (range.max - range.min + 1)
-//             * (input - range.min));
-//         break;
-//     }
-// }
-
-// if (result !== undefined) {
-//     console.log(result);
-// } else {
-//     console.log('Value is out of range');
-// }
-
-// console.log(ranges[0].min);
-
-var a = 60*(4000/330);
-console.log(a);
+if (pattern.test(dateS)) {
+    console.log("if문들어왔어요");
+} else {
+    console.log("안들어왔어요");
+}
