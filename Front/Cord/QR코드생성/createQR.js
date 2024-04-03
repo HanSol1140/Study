35be985ad8b43ee71cbaa78492399cfac5488a51
songhansol <skycriperd@gmail.com>
2024-04-03 19:06:33 +0900

    123123

--- Front/Cord/QR코드생성/createQR.js
+++ Front/Cord/QR코드생성/createQR.js
@@ -1,22 +1,26 @@
+// npm i qrcode
 const QRCode = require('qrcode');
 
-// 전화번호 설정
-const phoneNumber = '07040994350'; // 여기에 QR 코드를 통해 전화를 걸 전화번호를 입력하세요.
-const telLink = `tel:${phoneNumber}`;
+// const qrCodeData = `tel:01031277711`;
+const qrCodeData = `https://nanonix.lol`;
 
-// QR 코드 생성
-QRCode.toFile(
-  'phoneQRCode.png', // QR 코드 이미지 파일 이름
-  telLink, // QR 코드 데이터
-  {
-    width: 300, // QR 코드 이미지의 너비를 300픽셀로
-    color: {
-      dark: '#000000', // QR 코드의 색상
-      light: '#ffffff' // QR 코드 배경의 색상
-    }
+const qrCodeOptions = {
+  errorCorrectionLevel: 'H',
+  type: 'image/png',
+  quality: 1,
+  margin: 1,
+  width: 300,
+  color: {
+    dark: '#000000FF',
+    light: '#FFFFFFFF',
   },
-  function (err) {
-    if (err) throw err;
+};
+
+QRCode.toFile('phoneQRCode.png', qrCodeData, qrCodeOptions, (err) => {
+  if (err) {
+    console.error('QR 코드 생성 중 오류가 발생했습니다:', err);
+  } else {
     console.log('QR 코드가 성공적으로 생성되었습니다.');
   }
-);
+});
+// https://nanonix.lol
---
+++ Front/Cord/QR코드생성/createQR_generator.js
@@ -0,0 +1,29 @@
+const qr = require('qrcode-generator');
+const sharp = require('sharp');
+const fs = require('fs');
+
+// 전화번호 설정
+const phoneNumber = '07040994350';
+const telLink = `tel:07040994350`;
+
+// QR 코드 생성 (SVG 형식)
+let qrCode = qr(4, 'L');
+qrCode.addData(telLink);
+qrCode.make();
+let qrSvg = qrCode.createSvgTag({});
+
+// SVG를 PNG로 변환하고 너비 지정하여 파일로 저장
+const svgBuffer = Buffer.from(qrSvg);
+const outputPath = 'phoneQRCode.png'; // PNG 파일 이름
+const width = 300; // 원하는 이미지의 너비 (픽셀 단위)
+
+sharp(svgBuffer)
+  .png()
+  .resize(width) // 너비 지정, 높이는 자동 조정
+  .toFile(outputPath, (err, info) => {
+    if (err) {
+      console.error('QR 코드 생성 중 오류가 발생했습니다:', err);
+    } else {
+      console.log(`QR 코드가 ${width}px 너비로 PNG 형식으로 성공적으로 생성되었습니다.`, info);
+    }
+  });
