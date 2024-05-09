18b5dd59da9e8d042a4cbcc5c0e7346981dc6f09
songhansol <songhansol1140@gmail.com>
2024-05-09 18:34:53 +0900

    123

---
+++ nestjs/02_restapi/src/movies/movies.controller.ts
@@ -0,0 +1,45 @@
+import { Controller, Delete, Get, Param, Patch, Post, Body, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+    constructor(private readonly moviesService: MoviesService){}
+    
+    @Get()
+    getAll() :Movie[] {
+        return this.moviesService.getAll();
+    };
+
+        
+    @Get("search")
+    search(@Query('year') searchingYear: string){
+        return `We are searching for a movie a made after: ${searchingYear}`
+    }
+
+    @Get('/:id')
+    getOne(@Param('id') movieId: string) {
+        return this.moviesService.getOne(movieId);
+    };
+
+    @Post()
+    create(@Body() movieData) {
+        return this.moviesService.create(movieData);
+    };
+
+    @Delete("/:id")
+    remove(@Param('id') movieId:string) {
+        return this.moviesService.deleteOne(movieId);
+    };
+
+    @Patch("/:id")
+    patch(@Param('id') movieId:string, @Body() updateData){
+        return {
+            updatedMovie: movieId,
+            ...updateData,
+        }
+    }
+
+
+
+}   
