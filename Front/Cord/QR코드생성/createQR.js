97f51411d6b4fd181d3999b291da64589407df25
songhansol <songhansol1140@gmail.com>
2024-11-15 09:13:00 +0900

    123123

--- Front/Cord/QR코드생성/createQR.js
+++ Front/Cord/QR코드생성/createQR.js
@@ -1,29 +1,29 @@
-// npm i qrcode
 const QRCode = require('qrcode');
+const path = require('path');
 
-const qrCodeData = `tel:070-4099-4350`;
-// const qrCodeData = `hsadfsadfsasadfasdf`;
-// const qrCodeData = `http://nanonix.lol`;
-// const qrCodeData = `www.naver.com`;
-// const qrCodeData = `http://192.168.0.137:3000/CallPage`;
+// QR 코드에 넣을 데이터
+const qrCodeData = `https://artvizual.com/`;
 
+// QR 코드 옵션 설정
 const qrCodeOptions = {
   errorCorrectionLevel: 'H',
   type: 'image/jpg',
   quality: 1,
   margin: 1,
-  width: 300,
+  width: 500,
   color: {
     dark: '#000000FF',
     light: '#FFFFFFFF',
   },
 };
 
-QRCode.toFile('phoneQRCode.jpg', qrCodeData, qrCodeOptions, (err) => {
+// __dirname을 사용하여 현재 폴더에 QR 코드 파일 생성
+const filePath = path.join(__dirname, "QR" + ".jpg");
+
+QRCode.toFile(filePath, qrCodeData, qrCodeOptions, (err) => {
   if (err) {
     console.error('QR 코드 생성 중 오류가 발생했습니다:', err);
   } else {
-    console.log('QR 코드가 성공적으로 생성되었습니다.');
+    console.log('QR 코드가 성공적으로 생성되었습니다:', filePath);
   }
 });
-// https://nanonix.lol
